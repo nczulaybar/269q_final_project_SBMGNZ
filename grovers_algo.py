9d05f976a3baa2b8f1bfc444afe3f7ddcd41d324
nczulaybar <nczulaybar@gmail.com>
2019-06-01 02:23:08 -0700

    fully operational!

--- grovers_algo.py
+++ grovers_algo.py
@@ -3,68 +3,60 @@ from pyquil.gates import *
 from pyquil.quil import DefGate
 import numpy as np
 
+from pyquil.api import WavefunctionSimulator
+wf_sim = WavefunctionSimulator()
+
 # Create core operation for diffusion step
 def create_diffusion_op(n):
-	a = 2.0 * np.full((2**n, 2**n), 1/(2**n)) - np.eye(2**n)
+	a = 2.0 * np.full((2**n, 2**n), 1/(2**n)) - np.identity(2**n)
 	diff_def = DefGate("DIFF", a)
 	DIFF = diff_def.get_constructor()
 	return diff_def, DIFF
 
-def grovers(num_qubits: int, oracle, rounds = -1):
-	qc = get_qc(str(num_qubits) + 'q-qvm')
+def grovers(N: int, begin: str, end: str, rounds = -1):
+	ancilla_qbits = 0
+	debug_qbits = 0
+	total_qbits = N + ancilla_qbits + debug_qbits
+
+	qc = get_qc(str(total_qbits) + 'q-qvm')
 	p = Program()
 
-	diff_def, diffusion_op = create_diffusion_op(num_qubits)
+	### Make Gate Def'ns ###
+	diff_def, diffusion_op = create_diffusion_op(N)
+	
+	### Make program def'ns ###
+	oracle = make_oracle(begin, end, N)
+	
+	### Add gates def'n ###
 	p += diff_def
 
-	# oracle_def = DefGate("ORACLE", oracle)
-	# ORACLE = oracle_def.get_constructor()
-	# p += oracle_def
-	p += oracle
-
-	diff_args = range(num_qubits)
-
 	# initalize state of non-ancilary qubits
-	for i in range(num_qubits):
+	for i in range(N):
 		p += H(i)
 
 	# default number of rounds
 	if rounds == -1:
-		rounds = int(np.pi/4 * np.sqrt(2**num_qubits))
+		rounds = int(np.pi/4 * np.sqrt(2**N))
 	print("Rounds: " + str(rounds))
 
-	for r in range(rounds):
-		# appy oracle
-		p += oracle #ORACLE(*diff_args)
-		p += diffusion_op(*diff_args)
-		
+	for r in range(2): #debug, rounds -> 1
+		p += oracle #Oracle isn't gate, contains subcircuits
+		p += diffusion_op(*range(N)) 
 
-	results = qc.run_and_measure(p, trials = 10)
-	for i in range(num_qubits):
+		# # simulator debug
+		# wavefunction = wf_sim.wavefunction(p)
+		# print(wavefunction)
+
+	# Real run
+	results = qc.run_and_measure(p, trials = 30)
+	for i in range(total_qbits):
 		print("qubit " + str(i) +  ": " + str(results[i]))
-
-# """
-# Notes:
-# 	1) oracle should be a numpy array
-# 	2) make sure not to run for more than the suggested number of rounds, 
-# 	   because the accuracy falls off (I'm not quite sure why)
-
-# """
-# SEARCHED_STRING = "1101"
-# N = len(SEARCHED_STRING)
-# oracle = np.zeros(shape=(2 ** N, 2 ** N))
-# for b in range(2 ** N):
-#     if np.binary_repr(b, N) == SEARCHED_STRING:
-#         oracle[b, b] = -1
-#     else:
-#         oracle[b, b] = 1
 
 # Construct diag operator that returns -1 if all 
 # gates are 1
 def create_Nbit_CZ(N: int):
 	my_mat = np.identity(2 ** N)
 	my_mat[2 ** N - 1, 2 ** N - 1] = -1
-	#my_mat[0,0] = -1
 	Nbit_CZ_def = DefGate("Nbit_CZ", my_mat)
 	return Nbit_CZ_def, Nbit_CZ_def.get_constructor()
 
@@ -77,18 +69,18 @@ def make_oracle(begin: str, end: str, N: int):
 
 	# Compute key XOR begin
 	for i in range(N):
-		if begin[i] == 0:
+		if begin[i] == "0":
 			oracle += I(int(i))
 
-		elif begin[i] == 1:
+		elif begin[i] == "1":
 			oracle += X(int(i))
 
 	# Check prev result == end
 	for i in range(N):
-		if end[i] == 0:
+		if end[i] == "0":
 			oracle += X(int(i))
 
-		elif end[i] == 1:
+		elif end[i] == "1":
 			oracle += I(int(i))
 
 	# If we have all one's, i.e, we have a match, invert phase
@@ -101,25 +93,19 @@ def make_oracle(begin: str, end: str, N: int):
 
 	# Undo our previous modifications to input
 	for i in range(N):
-		if end[i] == 0:
+		if end[i] == "0":
 			oracle += X(int(i))
 
-		elif end[i] == 1:
+		elif end[i] == "1":
 			oracle += I(int(i))
 
 	for i in range(N):
-		if begin[i] == 0:
+		if begin[i] == "0":
 			oracle += I(int(i))
 
-		elif begin[i] == 1:
+		elif begin[i] == "1":
 			oracle += X(int(i))
 
 	return oracle
 
-
-oracle = make_oracle("11", "01", 2)
-
-grovers(2, oracle)
-
-
-
+grovers(4, "0011", "0110")
