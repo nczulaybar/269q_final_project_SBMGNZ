0fe09b7ddb9602f85fbb99ee916bb87104576189
Sawyerb <sawyerb@stanford.edu>
2019-06-02 12:58:50 -0700

    added support for shift rows

--- grovers_algo.py
+++ grovers_algo.py
@@ -7,7 +7,7 @@ from pyquil.api import WavefunctionSimulator
 wf_sim = WavefunctionSimulator()
 
 # Builds Grover's algorithm program
-def run_grovers(N: int, begin: str, end: str, rounds = -1):
+def run_grovers(N: int, begin: str, end: str, rounds = -1, offsets = [1, 0]):
 	
 	qc = get_qc(str(N) + 'q-qvm')
 
@@ -24,7 +24,7 @@ def run_grovers(N: int, begin: str, end: str, rounds = -1):
 
 	diff_def, diffusion_op = create_diffusion_op(N)
 	p += diff_def
-	oracle = make_oracle(begin, end, N)
+	oracle = make_shift_rows_oracle(begin, end, N, offsets)
 
 	# Main Loop
 	for r in range(rounds): #debug, rounds -> 1
@@ -81,6 +81,126 @@ def make_oracle(begin: str, end: str, N: int):
 
 	return oracle
 
+def make_shift_rows_oracle(begin: str, end: str, N: int, offsets: list):
+	oracle = Program()
+
+	### Gate defs 
+	Nbit_CZ_def, Nbit_CZ = create_Nbit_CZ(N)
+	oracle += Nbit_CZ_def
+
+	rows = int(np.sqrt(N))
+	#rows = 1
+	print(rows)
+	cols = int(N/rows)
+
+	for i in range(rows):
+		oracle += shift_row_p1(begin[i*cols: (i+1)*cols], end[i*cols: (i+1)*cols],
+							cols, offsets[i], i*cols)
+	
+	oracle += 	Nbit_CZ(*(range(N)))
+	
+	for i in range(rows):
+		oracle += shift_row_p2(begin[i*cols: (i+1)*cols], end[i*cols: (i+1)*cols],
+					cols, offsets[i], i*cols)
+
+	return oracle
+
+# Builds oracle
+def shift_row_p1(begin: str, end: str, N: int, offset: int, start: int):
+
+	oracle = Program()
+
+	# Compute key XOR begin
+	for i in range(N):
+		if begin[i] == "0":
+			oracle += I(start + int((i+offset)%N))
+
+		elif begin[i] == "1":
+			oracle += X(start + int((i+offset)%N))
+
+	# Check prev result == end
+	for i in range(N):
+		if end[i] == "0":
+			oracle += X(start + int(i))
+
+		elif end[i] == "1":
+			oracle += I(start + int(i))
+
+	return oracle
+
+
+def shift_row_p2(begin: str, end: str, N: int, offset: int, start: int):
+	oracle = Program()
+
+	# Undo our previous modifications to input
+	for i in range(N):
+		if end[i] == "0":
+			oracle += X(start + int(i))
+
+		elif end[i] == "1":
+			oracle += I(start + int(i))
+
+	for i in range(N):
+		if begin[i] == "0":
+			oracle += I(start + int((i+offset)%N))
+
+		elif begin[i] == "1":
+			oracle += X(start + int((i+offset)%N))
+
+	return oracle
+
+# Builds oracle
+def shift_row_offset(begin: str, end: str, N: int, offset: int, start: int):
+
+	oracle = Program()
+	print("shift_row")
+	print(begin)
+	print(end)
+	print(N)
+	print(offset)
+	print(start)
+
+	### Gate defs 
+	Nbit_CZ_def, Nbit_CZ = create_Nbit_CZ(N)
+	oracle += Nbit_CZ_def
+
+	# Compute key XOR begin
+	for i in range(N):
+		if begin[i] == "0":
+			oracle += I(start + int((i+offset)%N))
+
+		elif begin[i] == "1":
+			oracle += X(start + int((i+offset)%N))
+
+	# Check prev result == end
+	for i in range(N):
+		if end[i] == "0":
+			oracle += X(start + int(i))
+
+		elif end[i] == "1":
+			oracle += I(start + int(i))
+
+	# If we have all one's, i.e, we have a match, invert phase
+	oracle += Nbit_CZ(*(range(start, start + N)))
+
+	# Undo our previous modifications to input
+	for i in range(N):
+		if end[i] == "0":
+			oracle += X(start + int(i))
+
+		elif end[i] == "1":
+			oracle += I(start + int(i))
+
+	for i in range(N):
+		if begin[i] == "0":
+			oracle += I(start + int((i+offset)%N))
+
+		elif begin[i] == "1":
+			oracle += X(start + int((i+offset)%N))
+
+	return oracle
+
+
 # Create core operation for diffusion step
 def create_diffusion_op(n):
 	a = 2.0 * np.full((2**n, 2**n), 1/(2**n)) - np.identity(2**n)
@@ -95,5 +215,5 @@ def create_Nbit_CZ(N: int):
 	Nbit_CZ_def = DefGate("Nbit_CZ", my_mat)
 	return Nbit_CZ_def, Nbit_CZ_def.get_constructor()
 
-run_grovers(4, "0011", "0110")
+run_grovers(4, "1011", "1100", offsets = [1, 0])
 
